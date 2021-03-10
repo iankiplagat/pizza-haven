e237d005c493f5b8f63fcd13e78af87b5651fca3
Ian Kiplagat <ianjkiplagat@gmail.com>
2021-03-10 18:38:11 +0300

    add order functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,110 @@
-// Form Validation
-$("form#form").on("submit", function (event) {
-  event.preventDefault();
-  let firstName = $("input#fname").val();
-  let lastName = $("input#lname").val();
-  let message = $("textarea#subject").val();
-
-  if ($("input#fname").val() && $("input#lname").val()) {
-    let alertMessage = `Hey ${firstName} , Thanks for reaching out, we'll get back to you shortly...good luck!`;
-    Swal.fire({
-      icon: "success",
-      title: "Cheers...",
-      text: alertMessage,
-    });
-  }
-});
-
-function myFunction() {
-  document.getElementById("deliver-order").style.display = "block";
+// Business logic
+function Pizza(type, size, crust, toppings) {
+  this.type = type;
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
 }
 
-function myFunctionOne() {
-  document.getElementById("deliver-order").style.display = "none";
-}
+Pizza.prototype.sizes = function () {
+  if (this.size === "Small") {
+    return 500;
+  } else if (this.size === "Medium") {
+    return 800;
+  } else if (this.size === "Large") {
+    return 1000;
+  } else alert("");
+};
+
+Pizza.prototype.crusts = function () {
+  if (this.crust === "Crispy") {
+    return 200;
+  } else if (this.size === "Stuffed") {
+    return 250;
+  } else if (this.size === "Glutten-free") {
+    return 300;
+  } else alert("");
+};
+
+Pizza.prototype.toppings = function () {
+  if (this.toppings === "Exta Cheese") {
+    return 200;
+  } else if (this.toppings === "Mushroom") {
+    return 200;
+  } else if (this.toppings === "Beef") {
+    return 150;
+  } else if (this.toppings === "Chicken") {
+    return 250;
+  } else alert("");
+};
+
+// User interface logic
+
+// Order
 
 function order() {
-  // document.getElementById("order-summary").innerHTML = Your order is
-}
-
-function checkout() {
-  document.getElementById("final-order");
-  prompt("Please enter your phone number");
-  alert(
-    "Your order is being processed. It will be delivered to your location soon if delivery option was selected. Thank you"
+  document.getElementById("order-summary").style.display = "block";
+  var totals = Pizza();
+  append(
+    "You have Ordered " +
+      Quantity("") +
+      " pizza," +
+      "" +
+      " amounting to ksh. " +
+      totals +
+      "" +
+      " Thanks for your order welcome again."
   );
+
+  $(document).ready(function () {
+    $("form#new-contact").submit(function (event) {
+      event.preventDefault();
+
+      var selectedPizzaName = $("select#pizzaType").val();
+      var selectedPizzaSize = $("select#pizzaSize").val();
+      var selectedPizzaCrust = $("select#pizzaCrust").val();
+      var selectedPizzaToppings = $("select#pizzaToppings").val();
+
+      var newOrder = new Pizza(type, size, crust, toppings);
+    });
+  });
+
+  function myFunction() {
+    document.getElementById("deliver-order").style.display = "block";
+  }
+
+  function myFunctionOne() {
+    document.getElementById("deliver-order").style.display = "none";
+  }
+
+  // $("#order-summary").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+  //   $("input#new-first-name").val("");
+  //   $("input#new-last-name").val("");
+  // }
+
+  function checkout() {
+    document.getElementById("final-order");
+    prompt("Please enter your phone number");
+    alert(
+      "Your order is being processed. It will be delivered to your location soon if delivery option was selected. Thank you"
+    );
+  }
+
+  // Form Validation
+  $("form#form").on("submit", function (event) {
+    event.preventDefault();
+    let firstName = $("input#fname").val();
+    let lastName = $("input#lname").val();
+    let message = $("textarea#subject").val();
+
+    if ($("input#fname").val() && $("input#lname").val()) {
+      let alertMessage = `Hey ${firstName} , Thanks for reaching out, we'll get back to you shortly...good luck!`;
+      Swal.fire({
+        icon: "success",
+        title: "Cheers...",
+        text: alertMessage,
+      });
+    }
+  });
 }
-
-// function Pizza(type, size, crust, toppings) {
-//   this.type = type;
-//   this.size = size;
-//   this.crust = crust;
-//   this.toppings = toppings;
-// }
-
-// Pizza.prototype.sizes = {
-//   small: 500,
-//   medium: 800,
-//   large: 1000,
-// };
-// Pizza.prototype.crusts = {
-//   crispy: 200,
-//   stuffed: 250,
-//   glutten: 300,
-// };
-// Pizza.prototype.toppingsList = {
-//   extraCheese: 200,
-//   mushroom: 200,
-//   beef: 150,
-//   chicken: 250,
-// };
